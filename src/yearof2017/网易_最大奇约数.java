961de37e63c844ce5f9e10ad65ca1f4f4f08b65c
SMILEXZW <1114998422@qq.com>
2019-02-25 09:50:20 +0800

    网易_最大奇约数

---
+++ src/yearof2017/网易_最大奇约数.java
@@ -0,0 +1,29 @@
+package yearof2017;
+
+import java.util.Scanner;
+
+/**
+ * @Auther: xuzhangwang
+ * @Description:
+ */
+public class 网易_最大奇约数 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(getSum(n));
+    }
+
+    public static long getSum(long n) {
+        long sum = 0;
+        while (n!=0) {
+            if (n%2==0) {
+                sum += (n*n)>>2;
+                n = n>>1;
+            } else {
+                sum += ((n+1)*(n+1))>>2;
+                n = n>>1;
+            }
+        }
+        return sum;
+    }
+}
