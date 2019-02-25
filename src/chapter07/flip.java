1d29b534c8f3eacb9b2e7d96f6a7d38a12d42b39
SMILEXZW <1114998422@qq.com>
2019-02-25 09:20:14 +0800

    不用任何比判断找出两个数字中较大的数

---
+++ src/chapter07/flip.java
@@ -0,0 +1,15 @@
+package chapter07;
+
+/**
+ * @Auther: xuzhangwang
+ * @Description: 不用任何比判断找出两个数字中较大的数
+ */
+public class flip {
+    public static void main(String[] args) {
+
+    }
+    public static int flip(int n) {
+//        return flip();
+        return 0;
+    }
+}
