c68e574cb3b796badedf2714a0608870eecbec16
SMILEXZW <1114998422@qq.com>
2019-02-25 09:48:35 +0800

    网易_合唱团

---
+++ src/yearof2017/网易_合唱团.java
@@ -0,0 +1,31 @@
+package yearof2017;
+
+/**
+ *
+ * @ClassName: WangYi_Choir
+ * @Description: 有 n 个学生站成一排，每个学生有一个能力值，牛牛想从这 n 个学生中按照顺序选取 k 名学生，
+ * 要求相邻两个学生的位置编号的差不超过 d，使得这 k 个学生的能力值的乘积最大，你能返回最大的乘积吗？
+ * @author xuzhangwang
+ * @date 2018年9月26日
+ *
+ */
+public class 网易_合唱团 {
+    /**
+     *
+     * @Title: getMaxChoir
+     * @Description: n 个学生中按照顺序选取 k 名学生，要求相邻两个学生的位置编号的差不超过 d，
+     *               使得这 k 个学生的能力值的乘积最大
+     * @param @param nums
+     * @param @param k
+     * @param @param d
+     * @param @return    参数
+     * @return int    返回类型
+     * @throws
+     */
+    public static int getMaxChoir(int[] nums, int k, int d) {
+        return 1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
