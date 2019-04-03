113f6f540cad19fe5fbb04ad79480a30d4622b4b
SMILEXZW <1114998422@qq.com>
2019-04-03 21:32:37 +0800

    合并两个有序链表-链表（基础）

---
+++ src/leetcode/_021.java
@@ -0,0 +1,95 @@
+package leetcode;
+
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * @Auther: xuzhangwang
+ * @Description:21. 合并两个有序链表
+ * 题目描述
+ * 评论 (302)
+ * 题解
+ * 提交记录
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class _021 {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = null;
+
+        ListNode n1 = new ListNode(0);
+        ListNode n2 = new ListNode(3);
+        ListNode n3 = new ListNode(4);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = null;
+        ListNode head = mergeTwoLists2(l1, n1);
+        print(head);
+    }
+
+    private static void print(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    /**
+     * 使用递归的方法
+     * @param l1
+     * @param l2
+     * @return
+     * 时间复杂度为 O（N）空间复杂度为 O（N）
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+    /**
+     * 使用迭代的方法
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = new ListNode(l1.val);
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = new ListNode(l2.val);
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        } else {
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+}
