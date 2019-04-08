f2126481888edfef023e14a960bb27b0eabcc322
SMILEXZW <1114998422@qq.com>
2019-04-08 10:36:05 +0800

    乘积最大子序列 - 必须掌握

--- src/leetcode/_152.java
+++ src/leetcode/_152.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
  */
 public class _152 {
     public static void main(String[] args) {
-        int[] nums = {0, 1, 0, 2, 3, 12};
+        int[] nums = {0, 0, 1, 0, 2, 3, 12};
         moveZeroes(nums);
         System.out.println(Arrays.toString(nums));
     }
